d933eb2b63b32062118f599a3bc5ba2ce3b2d569
xiangcatherine <xiang.catherine@gmail.com>
2017-06-13 18:50:05 -0400

    Update curl scripts

--- scripts/create_mood.sh
+++ scripts/create_mood.sh
@@ -1,9 +1,9 @@
 curl --include --request POST http://localhost:4741/moods \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=BAhJIiVhZGY4NGY0NzlhMTg3MWFiNWNhZDBmNDU1OTQzMzM0ZAY6BkVG--a57c539669897d0d26cc194181451e88eafbfed6" \
+  --header "Authorization: Token token=BAhJIiViYzc2ODE0Zjc2YTY5ZjA1MDI5NjRlNTU2YzJiOGU2OAY6BkVG--8a6aa19e2fa4041fd4686a9025c3d376b70feb9a" \
   --data '{
     "mood": {
-      "color_id": 7,
-      "note": "what a day"
+      "color_id": 10,
+      "note": "Rose all day"
     }
   }'
--- scripts/update-mood-notes.sh
+++ scripts/update-mood-notes.sh
@@ -2,10 +2,9 @@
 
 curl --include --request PATCH http://localhost:4741/moods/26 \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=BAhJIiVhZGY4NGY0NzlhMTg3MWFiNWNhZDBmNDU1OTQzMzM0ZAY6BkVG--a57c539669897d0d26cc194181451e88eafbfed6" \
+  --header "Authorization: Token token=BAhJIiViYzc2ODE0Zjc2YTY5ZjA1MDI5NjRlNTU2YzJiOGU2OAY6BkVG--8a6aa19e2fa4041fd4686a9025c3d376b70feb9a" \
   --data '{
     "mood": {
-      "user_id": 2,
-      "note": "OMG"
+      "note": "QUAT"
     }
   }'
