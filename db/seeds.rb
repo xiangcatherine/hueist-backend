98b7720a43ccab433cd3cd8f08e38f9180bb64cb
xiangcatherine <xiang.catherine@gmail.com>
2017-06-16 00:35:06 -0400

    Add seeds for color list

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+color_list = [
+  '#C71B20',
+  '#DA4120',
+  '#EB6A28',
+  '#FF9A2D',
+  '#FCBE2E',
+  '#FFE025',
+  '#D21B78',
+  '#EB1F84',
+  '#FF5D9F',
+  '#C5A0CF',
+  '#A0A8C9',
+  '#9ACCC2',
+  '#B0D4A2',
+  '#E3DAAB',
+  '#FADAA1',
+  '#ECA7B6',
+  '#E5B1A9',
+  '#F7CDA3',
+  '#C130E3',
+  '#8034DE',
+  '#3A39ED',
+  '#3D8AFF',
+  '#33C4EF',
+  '#32E3BE',
+  '#289C18',
+  '#1FBF43',
+  '#25D980'
+]
+
+color_list.each do |hex_value|
+  Color.create(hex_value: hex_value)
+end
